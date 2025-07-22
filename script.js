ed3eabe33bd4f1e32bb7c9a105502df83f106be5
Diego <iwujigoodluck5@gmail.com>
2025-07-22 08:53:56 +0000

    done the javascript

--- script.js
+++ script.js
@@ -1,22 +1,54 @@
 `use strict`;
 
-const container = document.querySelector(".container");
-
+const container = document.querySelector("#container");
 let dataJson;
 
-fetch("data.json")
-  .then((response) => response.json())
-  .then((data) => {
-    data.forEach((element) => {
-      console.log(element);
+const FetchData = async function () {
+  const getData = await fetch("data.json");
+  console.log(getData);
 
-      let string = `<div class="text-sm bg-white border">
-                        <p>${element.title}</p>
-                        <p>${element.title}</p>
-                        <p>${element.title}</p>
-                        <p>${element.title}</p>
-                    </div>`;
+  const changed = getData.json();
+  changed
+    .then((res) => {
+      dataJson = res;
+      return dataJson;
+    })
+    .then((data) => {
+      data.forEach((element) => {
 
-      container.insertAdjacentHTML("beforeend", string);
+        console.log(element);
+
+        const StringDiv = `
+      <div class="relative bg-[#f1c65b] rounded-2xl overflow-y-hidden">
+        <div class="relative h-8 overflow-hidden">
+          <img
+            class="size-12 right-2 block absolute -top-1"
+            src="${element.icon}"
+            alt=""
+          />
+        </div>
+        <div class="p-4 bg-[#1c1f4a] rounded-2xl">
+          <div class="flex justify-between items-center">
+            <p class="font-semibold text-white">${element.title}</p>
+            <img
+              class="h-1 cursor-pointer"
+              src="images/icon-ellipsis.svg"
+              alt=""
+            />
+          </div>
+          <div
+            class="flex-1 flex justify-between items-center md:flex-col md:items-start text-white"
+          >
+            <p class="text-xl">${element.timeframes["weekly"].current}hrs</p>
+            <span class="text-sm">Last Week - ${element.timeframes["weekly"].previous}hrs</span>
+          </div>
+        </div>
+      </div>
+`;
+        container.insertAdjacentHTML("beforeend", StringDiv)
+        
+      });
     });
-  });
+};
+
+FetchData();
